6eb174a24d3c97feaba36b9d862063871299d790
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-15 00:38:14 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Auth } from './pages/auth'
+import { ExpenseTracker } from './pages/expense-tracker/index'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom'
 
 const App = () => {
-    return <div></div>
+    const route = createBrowserRouter(
+        createRoutesFromElements(
+            <Route>
+                <Route path="/" element={<Auth />} />
+                <Route path="/expense-tracker" element={<ExpenseTracker />} />
+            </Route>
+        )
+    )
+
+    return <RouterProvider router={route} />
 }
 
 export default App
