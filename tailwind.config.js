4cd916c18bfbb43dcf6984a1022a500cfffaa8fa
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-15 00:38:26 +0530

    other changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                'primary-green': '#58ac84',
+            },
+            fontFamily: {
+                montserrat: "'Montserrat', sans-serif",
+            },
+            container: {
+                center: true,
+                padding: '1.5em',
+            },
+        },
     },
-    plugins: []
+    plugins: [],
 }
