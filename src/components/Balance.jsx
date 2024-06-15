d67d57e9af6d5a3748162cab1ed1d21869962662
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-15 18:45:13 +0530

    dynamicaly update balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { TiArrowSortedUp, TiArrowSortedDown } from 'react-icons/ti'
+import { GlobalContext } from '../context/GlobalState'
 
 const Balance = () => {
+    const { transactions } = useContext(GlobalContext)
+
+    const expenseAmountList = transactions.map(
+        (transaction) =>
+            transaction.transactionType === 'expense' &&
+            transaction.transactionAmount
+    )
+
+    const expenseAmount = expenseAmountList
+        .reduce((total, amount) => (total += amount))
+        .toFixed(2)
+
+    const incomeAmountList = transactions.map(
+        (transaction) =>
+            transaction.transactionType === 'income' &&
+            transaction.transactionAmount
+    )
+
+    const incomeAmount = incomeAmountList
+        .reduce((total, amount) => (total += amount))
+        .toFixed(2)
+
+    const balanceAmount = (incomeAmount - expenseAmount).toFixed(2)
+
     return (
         <>
             <div className="flex flex-col gap-2">
                 <h2 className="font-medium md:text-lg">Your Balance</h2>
-                <p className="text-3xl md:text-5xl">LKR2000.00</p>
+                <p className="text-3xl md:text-5xl">LKR{balanceAmount}</p>
             </div>
 
             <div className="mt-6 flex gap-2">
@@ -15,14 +40,14 @@ const Balance = () => {
                         <h2 className="font-medium md:text-lg">Income</h2>
                         <TiArrowSortedUp className="text-primary-green text-4xl" />
                     </div>
-                    <p className="text-lg md:text-xl">LKR3000.00</p>
+                    <p className="text-lg md:text-xl">LKR{incomeAmount}</p>
                 </div>
                 <div className="w-full">
                     <div className="flex items-center gap-2">
                         <h2 className="font-medium md:text-lg">Expenses</h2>
                         <TiArrowSortedDown className="text-4xl text-red-600" />
                     </div>
-                    <p className="text-lg md:text-xl">-LKR1000.00</p>
+                    <p className="text-lg md:text-xl">-LKR{expenseAmount}</p>
                 </div>
             </div>
         </>
