6149490b953db9fde39ead11a9c909576fe4f3a4
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-16 01:18:31 +0530

    implement the submit form function

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,40 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAddTransaction } from '../hooks/useAddTransaction'
 
 const AddTransaction = () => {
     const { addTransaction } = useAddTransaction()
+    const [description, setDescription] = useState('')
+    const [transactionAmount, setTransactionAmount] = useState('')
+    const [transactionType, setTransactionType] = useState('expense')
+
+    const resetInput = () => {
+        setDescription('')
+        setTransactionAmount('')
+        setTransactionType('expense')
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(addTransaction('haircut', 300, 'expense'))
+        addTransaction(description, transactionAmount, transactionType)
+        resetInput()
     }
 
     return (
         <>
-            <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
-                <div className="flex flex-col gap-2">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <div className="flex flex-col gap-3">
                     <input
                         type="text"
                         name="description"
                         placeholder="description"
                         className="w-full rounded border border-zinc-800 p-2"
-                        // required
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
                     />
                     <input
                         type="number"
                         name="amount"
                         placeholder="amount"
                         className="w-full rounded border border-zinc-800 p-2"
-                        // required
+                        value={transactionAmount}
+                        onChange={(e) => setTransactionAmount(e.target.value)}
+                        required
                     />
                 </div>
-                <div className="flex gap-6">
+                <div className="ml-1 flex gap-6">
                     <div className="flex items-center gap-2">
                         <input
                             type="radio"
                             name="trans-type"
                             id="income"
                             value="income"
+                            checked={transactionType === 'income'}
+                            onChange={(e) => setTransactionType(e.target.value)}
+                            className="scale-125 cursor-pointer"
                         />
                         <label htmlFor="income">Income</label>
                     </div>
@@ -44,6 +61,9 @@ const AddTransaction = () => {
                             name="trans-type"
                             id="expense"
                             value="expense"
+                            checked={transactionType === 'expense'}
+                            onChange={(e) => setTransactionType(e.target.value)}
+                            className="scale-125 cursor-pointer"
                         />
                         <label htmlFor="expense">Expense</label>
                     </div>
