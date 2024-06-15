8677ad59cc4e717b86d01101c13ff47a426a9d18
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-15 18:42:28 +0530

    custom hooks for add transaction to firebase db

---
+++ src/hooks/useAddTransaction.js
@@ -0,0 +1,27 @@
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../config/firebase-config'
+import { useGetUserInfo } from './useGetUserInfo'
+
+export const useAddTransaction = () => {
+    const transactionCollectionRef = collection(db, 'transactions')
+
+    const { userId } = useGetUserInfo()
+
+    const addTransaction = async (
+        description,
+        transactionAmount,
+        transactionType
+    ) => {
+        console.log(userId, description, transactionAmount, transactionType)
+
+        addDoc(transactionCollectionRef, {
+            userId,
+            description,
+            transactionAmount,
+            transactionType,
+            createdAt: serverTimestamp(),
+        })
+    }
+
+    return { addTransaction }
+}
