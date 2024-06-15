fec08e6c2caed86969070bebe80d0bf144cede1f
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-16 01:17:17 +0530

    set transaction function

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,10 @@
 export default (state, action) => {
     switch (action.type) {
+        case 'SET_TRANSACTIONS':
+            return {
+                ...state,
+                transactions: action.payload,
+            }
         default:
             return state
     }
